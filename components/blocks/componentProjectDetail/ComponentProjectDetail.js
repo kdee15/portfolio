adfe8aca0c061100e4a12737ca1194c68b66a7ef
Kirk Daniels <kirkdaniels@me.com>
2022-11-16 11:10:09 +0200

    try

--- components/blocks/componentProjectDetail/ComponentProjectDetail.js
+++ components/blocks/componentProjectDetail/ComponentProjectDetail.js
@@ -9,10 +9,38 @@ import classes from "./ComponentProjectDetail.module.scss";
 
 function ComponentProjectDetail({ contentModule }) {
   const [mobileView, setMobileView] = useState();
+  const [isDetail, setDetail] = useState();
+  const [showDesks, setDesks] = useState();
+  const [showTablet, setTablet] = useState();
+  const [showMobi, setMobi] = useState();
+  const [isOn, setOn] = useState();
 
   useEffect(() => {
     setMobileView(isMobile);
   }, []);
+
+  const handleDetail = (e) => {
+    !isDetail ? setDetail(true) : setDetail();
+    !isOn ? setOn(true) : setOn();
+    setDesks();
+    setMobi();
+    setTablet();
+  };
+  const handleDesk = (e) => {
+    !showDesks ? setDesks(true) : setDesks();
+    setMobi();
+    setTablet();
+  };
+  const handleTablet = (e) => {
+    !showTablet ? setTablet(true) : setTablet();
+    setMobi();
+    setDesks();
+  };
+  const handleMobi = (e) => {
+    !showMobi ? setMobi(true) : setMobi();
+    setDesks();
+    setTablet();
+  };
 
   const data = contentModule;
   console.log("contentModule", contentModule);
@@ -27,13 +55,29 @@ function ComponentProjectDetail({ contentModule }) {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+  const settingsMobi = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
+  const settingsTablet = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
 
   return (
     <>
       <nav className={classes.oNav}>
         <span
-          className={`${classes.demoTab} ${classes.details} ${classes.show} ${classes.on}`}
-          name="details"
+          onClick={handleDetail}
+          className={`${classes.demoTab} ${classes.details} ${classes.show} ${
+            isDetail ? classes.on : classes.off
+          }`}
         >
           <figure className={classes.mIcon}>
             <svg
@@ -59,73 +103,87 @@ function ComponentProjectDetail({ contentModule }) {
           </figure>
           <p className={classes.aIconText}>Project Details</p>
         </span>
-        <span
-          className={`${classes.demoTab} ${classes.demoDesk} ${classes.show}`}
-          name="demo-desk"
-        >
-          <figure className={classes.mIcon}>
-            <svg
-              className={`${classes.aIcon} ${classes.laptop}`}
-              x="0px"
-              y="0px"
-              viewBox="0 0 148.2 100"
-            >
-              <g>
-                <path d="M148.2,5.9c0-3.4-2.7-5.9-5.9-5.9H5.9C2.5,0,0,2.7,0,5.9v83h148.2V5.9z M140.7,77H7.5V8.4h133.4L140.7,77L140.7,77z" />
-                <path
-                  d="M5,100h138.2c2.7,0,5-3,5-6.6v-3.9h-58v1.4c0,2-1.8,3.9-4.1,3.9H62c-2.3,0-4.1-1.8-4.1-3.9v-1.4H0v3.9C0,97,2.3,100,5,100z
+
+        {coverImageDesk ? (
+          <span
+            onClick={handleDesk}
+            className={`${classes.demoTab} ${classes.demoDesk} ${
+              classes.show
+            } ${showDesks ? classes.on : classes.off}`}
+          >
+            <figure className={classes.mIcon}>
+              <svg
+                className={`${classes.aIcon} ${classes.laptop}`}
+                x="0px"
+                y="0px"
+                viewBox="0 0 148.2 100"
+              >
+                <g>
+                  <path d="M148.2,5.9c0-3.4-2.7-5.9-5.9-5.9H5.9C2.5,0,0,2.7,0,5.9v83h148.2V5.9z M140.7,77H7.5V8.4h133.4L140.7,77L140.7,77z" />
+                  <path
+                    d="M5,100h138.2c2.7,0,5-3,5-6.6v-3.9h-58v1.4c0,2-1.8,3.9-4.1,3.9H62c-2.3,0-4.1-1.8-4.1-3.9v-1.4H0v3.9C0,97,2.3,100,5,100z
               "
-                />
-              </g>
-            </svg>
-          </figure>
-          <p className={classes.aIconText}>Desktop Screens</p>
-        </span>
-        <span
-          className={`${classes.demoTab} ${classes.demoTab}  ${classes.show}`}
-          name="demo-tab"
-        >
-          <figure className={classes.mIcon}>
-            <svg
-              className={`${classes.aIcon} ${classes.tablet}`}
-              x="0px"
-              y="0px"
-              viewBox="0 0 69.1 100"
-            >
-              <g>
-                <path
-                  d="M63.4,0H5.8C2.6,0,0,2.6,0,5.8v88.5c0,3.2,2.6,5.8,5.8,5.8h57.6c3.2,0,5.8-2.6,5.8-5.8V5.8C69.2,2.6,66.5,0,63.4,0z
+                  />
+                </g>
+              </svg>
+            </figure>
+            <p className={classes.aIconText}>Desktop Screens</p>
+          </span>
+        ) : null}
+        {coverImageTablet ? (
+          <span
+            onClick={handleTablet}
+            className={`${classes.demoTab} ${classes.demoTab}  ${
+              classes.show
+            } ${showTablet ? classes.on : classes.off}`}
+          >
+            <figure className={classes.mIcon}>
+              <svg
+                className={`${classes.aIcon} ${classes.tablet}`}
+                x="0px"
+                y="0px"
+                viewBox="0 0 69.1 100"
+              >
+                <g>
+                  <path
+                    d="M63.4,0H5.8C2.6,0,0,2.6,0,5.8v88.5c0,3.2,2.6,5.8,5.8,5.8h57.6c3.2,0,5.8-2.6,5.8-5.8V5.8C69.2,2.6,66.5,0,63.4,0z
                M34.6,97.4c-2.7,0-4.8-2.2-4.8-4.8c0-2.7,2.2-4.8,4.8-4.8c2.7,0,4.8,2.2,4.8,4.8C39.3,95.2,37.3,97.4,34.6,97.4z M63.5,83.8H5.6V6
               h57.8V83.8z"
-                />
-                <circle cx="34.6" cy="92.6" r="2.9" />
-              </g>
-            </svg>
-          </figure>
-          <p className={classes.aIconText}>Tablet Screens</p>
-        </span>
-        <span
-          className={`${classes.demoTab} ${classes.demoMob} ${classes.show}`}
-          name="demo-mob"
-        >
-          <figure className={classes.mIcon}>
-            <svg
-              className={`${classes.aIcon} ${classes.mobile}`}
-              x="0px"
-              y="0px"
-              viewBox="0 0 51.6 100"
-            >
-              <g>
-                <path
-                  d="M46.1,0H5.5C2.5,0,0,2.5,0,5.5v88.9c0,3.1,2.5,5.5,5.5,5.5h40.5c3.1,0,5.5-2.5,5.5-5.5V5.7C51.6,2.6,49.1,0,46.1,0z
+                  />
+                  <circle cx="34.6" cy="92.6" r="2.9" />
+                </g>
+              </svg>
+            </figure>
+            <p className={classes.aIconText}>Tablet Screens</p>
+          </span>
+        ) : null}
+        {coverImageMobile ? (
+          <span
+            onClick={handleMobi}
+            className={`${classes.demoTab} ${classes.demoMob} ${classes.show} ${
+              showMobi ? classes.on : classes.off
+            }`}
+          >
+            <figure className={classes.mIcon}>
+              <svg
+                className={`${classes.aIcon} ${classes.mobile}`}
+                x="0px"
+                y="0px"
+                viewBox="0 0 51.6 100"
+              >
+                <g>
+                  <path
+                    d="M46.1,0H5.5C2.5,0,0,2.5,0,5.5v88.9c0,3.1,2.5,5.5,5.5,5.5h40.5c3.1,0,5.5-2.5,5.5-5.5V5.7C51.6,2.6,49.1,0,46.1,0z
                M25.8,97.5c-2.6,0-4.6-2.1-4.6-4.6s2.1-4.6,4.6-4.6c2.6,0,4.6,2.1,4.6,4.6S28.4,97.5,25.8,97.5z M46.2,84.6H5.4V9.8h40.8V84.6z"
-                />
-                <circle cx="25.8" cy="92.9" r="2.8" />
-              </g>
-            </svg>
-          </figure>
-          <p className={classes.aIconText}>Mobile Screens</p>
-        </span>
+                  />
+                  <circle cx="25.8" cy="92.9" r="2.8" />
+                </g>
+              </svg>
+            </figure>
+            <p className={classes.aIconText}>Mobile Screens</p>
+          </span>
+        ) : null}
+
         <span
           className={`${classes.demoTab} ${classes.home} ${classes.show}`}
           href="<?php echo get_home_url(); ?>"
@@ -162,7 +220,7 @@ function ComponentProjectDetail({ contentModule }) {
       <main className={classes.oProjects}>
         <section className={classes.oProjectContent}>
           {mobileView ? (
-            <figure className={`${classes.oProjectCover} ${classes.on}`}>
+            <figure className={`${classes.oProjectCover}`}>
               <Image
                 className={`${classes.aImage} ${classes.aCoverMob} a-responsive-image`}
                 src={coverImageMobile?.url}
@@ -175,7 +233,11 @@ function ComponentProjectDetail({ contentModule }) {
               />
             </figure>
           ) : (
-            <figure className={`${classes.oProjectCover} ${classes.on}`}>
+            <figure
+              className={`${classes.oProjectCover} ${
+                !isOn ? classes.on : classes.off
+              }`}
+            >
               <Image
                 className={`${classes.aImage} ${classes.aCoverDesk} a-responsive-image`}
                 src={coverImageDesk?.url}
@@ -191,8 +253,9 @@ function ComponentProjectDetail({ contentModule }) {
         </section>
 
         <div
-          className={`${classes.oTab} ${classes.slideContent} ${classes.on} ${classes.details}`}
-          id="details"
+          className={`${classes.oTab} ${classes.slideContent} ${
+            classes.details
+          } ${!isDetail ? classes.on : classes.off}`}
         >
           <div
             className={`${classes.mDetail} ${classes.date} ${classes.dateStart}`}
@@ -219,8 +282,9 @@ function ComponentProjectDetail({ contentModule }) {
         </div>
 
         <div
-          className={`${classes.oTab} ${classes.slideContent}`}
-          id="demo-desk"
+          className={`${classes.oTab} ${classes.slideContent} ${
+            classes.demoDesk
+          } ${showDesks ? classes.on : classes.off}`}
         >
           <div className={classes.oWrapper}>
             <Slider {...settingsDesk}>
@@ -230,21 +294,32 @@ function ComponentProjectDetail({ contentModule }) {
             </Slider>
           </div>
         </div>
-
-        {/*      
-       
-        <div class="o-tab slide-content" id="demo-tab">
-          <div class="o-wrapper">
-            <div class="owl-tablet owl-carousel owl-theme">TABLET</div>
+        <div
+          className={`${classes.oTab} ${classes.slideContent} ${
+            classes.demoMobile
+          } ${showMobi ? classes.on : classes.off}`}
+        >
+          <div className={classes.oWrapper}>
+            <Slider {...settingsMobi}>
+              {carouselDeskScreens.map((item, index) => (
+                <ComponentCarouselImage item={item} key={index} />
+              ))}
+            </Slider>
           </div>
         </div>
-
-        <div class="o-tab slide-content" id="demo-mob">
-          <div class="o-wrapper">
-            <div class="owl-mobile owl-carousel owl-theme">MOBILE</div>
+        <div
+          className={`${classes.oTab} ${classes.slideContent} ${
+            classes.demoTablet
+          } ${showTablet ? classes.on : classes.off}`}
+        >
+          <div className={classes.oWrapper}>
+            <Slider {...settingsTablet}>
+              {carouselDeskScreens.map((item, index) => (
+                <ComponentCarouselImage item={item} key={index} />
+              ))}
+            </Slider>
           </div>
-        </div> 
-        */}
+        </div>
       </main>
     </>
   );
